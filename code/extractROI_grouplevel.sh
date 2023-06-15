9b83cd468342270b004a7568c376c76903970bab
orizaff <orizaff@udel.edu>
2023-06-15 17:39:02 -0400

    ran ppi all levels, extracted VS ROI activation for ANOVA

--- code/extractROI_grouplevel.sh
+++ code/extractROI_grouplevel.sh
@@ -12,10 +12,11 @@ for ROI in VS_thr5; do
 	outputdir=${maindir}/derivatives/imaging_plots
 	mkdir -p $outputdir
 
-	for COPENUM in 1 2; do
+	for COPENUM in 1 2 3 4 5 6; do
 		cnum_padded=`zeropad ${COPENUM} 2`
-		MAINOUTPUT=${maindir}/derivatives/fsl/L3_model-3_task-${TASK}_n45_flame1_SFN_edition
+		MAINOUTPUT=${maindir}/derivatives/fsl/L3_model-0_task-${TASK}_n23_flame1
 		DATA=`ls -1 ${MAINOUTPUT}/L3_task-${TASK}_type-${TYPE}_cnum-${cnum_padded}_*onegroup.gfeat/cope1.feat/filtered_func_data.nii.gz`
+		#DATA=`ls -1 ${MAINOUTPUT}/L3_task-${TASK}_type-${TYPE}_cnum-${COPENUM}_*onegroup.gfeat/cope1.feat/filtered_func_data.nii.gz`
 		fslmeants -i $DATA -o ${outputdir}/${ROI}_type-${TYPE}_cope-${cnum_padded}.txt -m ${MASK}
 
 	done
