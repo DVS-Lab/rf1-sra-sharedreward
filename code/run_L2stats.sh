0ea94679bd19603bd68cc6fcf1051ca804c9c07a
jen-yang <jen.youngjx@outlook.com>
2024-03-30 12:35:01 -0400

    linux to git update

--- code/run_L2stats.sh
+++ code/run_L2stats.sh
@@ -9,14 +9,14 @@ logs=$maindir/logs
 logfile=${logs}/rerunL2_date-`date +"%FT%H%M"`.log
 
 # the "type" variable below is setting a path inside the main script
-for type in "ppi_seed-eyeball_left" "ppi_seed-eyeball_right"; do
-#for type in "act"; do # "act" "ppi_seed-VS_thr5"
+#for type in "ppi_seed-eyeball_left" "ppi_seed-eyeball_right"; do
+for type in "act"; do # "act" "ppi_seed-VS_thr5"
 #for type in "ppi_seed-NAcc-bin"; do
 	for sub in `cat ${scriptdir}/sub_BothRuns.txt`; do
 #	for sub in 10827; do
 		# Manages the number of jobs and cores
   	SCRIPTNAME=${maindir}/code/L2stats.sh
-  	NCORES=10
+  	NCORES=4
   	while [ $(ps -ef | grep -v grep | grep $SCRIPTNAME | wc -l) -ge $NCORES ]; do
     		sleep 1s
   	done
