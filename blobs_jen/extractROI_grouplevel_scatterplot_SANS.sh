d83628aa96864cdd3f1a8cb959ca5dc3e1b9314d
jen-yang <jen.youngjx@outlook.com>
2024-04-02 20:58:00 -0400

    extract SANS group mean update

--- blobs_jen/extractROI_grouplevel_scatterplot_SANS.sh
+++ blobs_jen/extractROI_grouplevel_scatterplot_SANS.sh
@@ -9,7 +9,7 @@ for ROI in nppi-dmn_model-14_cnum-11_con13_z2; do
 #	MASK=${maindir}/masks/seed-${ROI}.nii.gz
 	MASK=${maindir}/masks/SANS/${ROI}.nii.gz
 	TASK=sharedreward
-	TYPE="nppi-dmn"
+	TYPE="nppi-dmn"	
 	N=93 # remember to change according to the model
 	model_N=14 # remember to change according to the model
 	outputdir=${maindir}/derivatives/group_means/SANS
@@ -45,7 +45,7 @@ for ROI in nppi-dmn_model-14_cnum-10_con15_z1; do
 	done
 done
 
-for nppi-dmn_model-13_cnum-11_con5_z1; do
+for ROI in nppi-dmn_model-13_cnum-11_con5_z1; do
 #	MASK=${maindir}/masks/seed-${ROI}.nii.gz
 	MASK=${maindir}/masks/SANS/${ROI}.nii.gz
 	TASK=sharedreward
@@ -65,7 +65,7 @@ for nppi-dmn_model-13_cnum-11_con5_z1; do
 	done
 done
 
-for nppi-dmn_model-13_cnum-10_con6_z1; do
+for ROI in nppi-dmn_model-13_cnum-10_con6_z1; do
 #	MASK=${maindir}/masks/seed-${ROI}.nii.gz
 	MASK=${maindir}/masks/SANS/${ROI}.nii.gz
 	TASK=sharedreward
@@ -85,3 +85,42 @@ for nppi-dmn_model-13_cnum-10_con6_z1; do
 	done
 done
 
+for ROI in thr_seed-VS; do
+#	MASK=${maindir}/masks/seed-${ROI}.nii.gz
+	MASK=${maindir}/masks/SANS/${ROI}.nii.gz
+	TASK=sharedreward
+	TYPE=act
+	N=93 # remember to change according to the model
+	model_N=1 # remember to change according to the model
+	outputdir=${maindir}/derivatives/group_means/SANS
+	mkdir -p $outputdir
+	
+	for COPENUM in 11 10; do #
+		cnum_padded=`zeropad ${COPENUM} 2`
+		MAINOUTPUT=${maindir}/derivatives/fsl/L3_task-${TASK}_model-${model_N}_type-${TYPE}_n${N}
+		DATA=`ls -1 ${MAINOUTPUT}/L3_task-${TASK}_type-${TYPE}_*cnum-${cnum_padded}_*onegroup.gfeat/cope1.feat/filtered_func_data.nii.gz`
+		#DATA=`ls -1 ${MAINOUTPUT}/L3_task-${TASK}_type-${TYPE}_cnum-${COPENUM}_*onegroup.gfeat/cope1.feat/filtered_func_data.nii.gz`
+		fslmeants -i $DATA -o ${outputdir}/${ROI}_type-${TYPE}_cope-${cnum_padded}.txt -m ${MASK}
+
+	done
+done
+
+for ROI in thr_seed-VS; do
+#	MASK=${maindir}/masks/seed-${ROI}.nii.gz
+	MASK=${maindir}/masks/SANS/${ROI}.nii.gz
+	TASK=sharedreward
+	TYPE="nppi-dmn"
+	N=93 # remember to change according to the model
+	model_N=1 # remember to change according to the model
+	outputdir=${maindir}/derivatives/group_means/SANS
+	mkdir -p $outputdir
+	
+	for COPENUM in 10 11; do #
+		cnum_padded=`zeropad ${COPENUM} 2`
+		MAINOUTPUT=${maindir}/derivatives/fsl/L3_task-${TASK}_model-${model_N}_type-${TYPE}_n${N}
+		DATA=`ls -1 ${MAINOUTPUT}/L3_task-${TASK}_type-${TYPE}_*cnum-${cnum_padded}_*onegroup.gfeat/cope1.feat/filtered_func_data.nii.gz`
+		#DATA=`ls -1 ${MAINOUTPUT}/L3_task-${TASK}_type-${TYPE}_cnum-${COPENUM}_*onegroup.gfeat/cope1.feat/filtered_func_data.nii.gz`
+		fslmeants -i $DATA -o ${outputdir}/${ROI}_type-${TYPE}_cope-${cnum_padded}.txt -m ${MASK}
+
+	done
+done
